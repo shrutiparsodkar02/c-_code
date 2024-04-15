2a214b48c834bca056d124851809a59cdadefb1e
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-04-15 19:21:39 +0530

    adding command line related exception code

---
+++ exceptioneg.java
@@ -0,0 +1,11 @@
+class Ex{
+	public static void main(String args[]){
+		System.out.println("opening file");
+		int n=args.length;
+		System.out.println("length-->"+n);
+		double a=45/n;
+		System.out.println("division--->"+a);
+		System.out.println("closing file");
+	}
+
+}
